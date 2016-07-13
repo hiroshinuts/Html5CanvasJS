14da549214af59c4d559b0b56b3a92335d9143ca
Rafael Hiroshi <rtagata@brq.com>
2016-07-13 16:17:53 -0300

    Orientacao a objetos JS

---
+++ Cap1/carro.js
@@ -0,0 +1,13 @@
+//Funcao construtora
+function Carro(cor, velocMaxima) {
+	this.cor = cor;
+	this.velocMaxima = velocMaxima;
+	this.velocAtual = 0;
+}
+
+//Prototype com os métodos
+Carro.prototype = {
+	acelerar: function() {
+		this.velocAtual += 10;
+	}
+}
