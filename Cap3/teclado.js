bb8dd0d4ed7244c078e738b8646e43a0f6bcb6a0
Rafael Hiroshi <rtagata@brq.com>
2016-07-14 11:38:13 -0300

    Movimento do objeto pelo teclado

---
+++ Cap3/teclado.js
@@ -0,0 +1,25 @@
+//Códigos de teclas - aqui vao todos os que forem necessarios
+var SETA_ESQUERDA = 37;
+var SETA_DIREITA = 39;
+
+function Teclado(elemento) {
+	this.elemento = elemento;
+
+	// Array de teclas pressionadas
+	this.pressionadas = [];
+
+	//Registrando o estado das teclas no array
+	var teclado = this;
+	elemento.addEventListener('keydown', function(evento) {
+		teclado.pressionadas[evento.keyCode] = true;
+	});
+	elemento.addEventListener('keyup', function(evento) {
+		teclado.pressionadas[evento.keyCode] = false;
+	});
+}
+
+Teclado.prototype = {
+	pressionada: function(tecla) {
+		return this.pressionadas[tecla];
+	}
+}
